9ab96826c6761900e2596f7818725516d9384d1a
tin33392js <calsharp0202@yahoo.com>
2023-04-23 05:17:45 +0700

    fix: must use useState for categories for conditional rendering and useEffect

--- components/App/FoodCategory.js
+++ components/App/FoodCategory.js
@@ -1,40 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import FoodFigure from './FoodFigure';
 import { useAppContext } from '@/context/useContext';
-import { useRef } from 'react';
 
 // display on specific types (types, diets, locations)
 export default function FoodCategory({ category }) {
   const [foods, setFoods] = useAppContext().foods;
-  const categories = useRef([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     if (foods?.length) {
-      categories.current = [
-        ...new Set(foods.map((f) => f.data()[category]).flat())
-      ];
+      setCategories([...new Set(foods.map((f) => f.data()[category]).flat())]);
     }
   }, [foods]);
 
   return (
     <>
-      {categories.current.slice(0, 10).map((cate) => (
-        <div key={cate}>
-          <h2 className="text-lg font-bold text-orange-500">{cate}</h2>
+      {categories.length > 1 &&
+        categories.slice(0, 10).map((cate) => (
+          <div key={cate}>
+            <h2 className="text-lg font-bold text-orange-500">{cate}</h2>
 
-          <div className="flex items-center gap-3 w-screen overflow-scroll">
-            {foods
-              .filter((f) => {
-                return f.data()[category].includes(cate);
-              })
-              .map((f) => {
-                console.log(f.id);
-                console.log(f);
-                return <FoodFigure doc={f} key={f.id} />;
-              })}
+            <div className="flex items-center gap-3 w-screen overflow-scroll">
+              {foods
+                .filter((f) => {
+                  return f.data()[category].includes(cate);
+                })
+                .map((f) => {
+                  console.log(f.id);
+                  console.log(f);
+                  return <FoodFigure doc={f} key={f.id} />;
+                })}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       {}
     </>
   );
