7d21cb36e8e8fea4beee51b9a3c20de761f2a211
tin33392js <calsharp0202@yahoo.com>
2023-04-23 09:28:44 +0700

    refactor: create component to fetch data and store to context

---
+++ components/common/InitStates.js
@@ -0,0 +1,32 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAppContext } from '@/context/useContext';
+import { firebase } from '@/firebase/clientApp';
+import { useEffect } from 'react';
+import { db } from '@/firebase/clientApp';
+import { useCollection, useDocument } from 'react-firebase-hooks/firestore';
+
+export default function InitStates() {
+  const [auth, authLoading, authError] = useAuthState(firebase.auth());
+  const [curUser, setCurUser] = useAppContext().curUser;
+
+  // need read from firestore, because hook not return custom prop (e.g. likedList)
+
+  // Docs: https://github.com/CSFrequency/react-firebase-hooks/tree/master/auth#useauthstate
+  const [foods, foodLoading, foodError] = useCollection(
+    db.collection('food_mock')
+  );
+  const [contextFoods, setFoods] = useAppContext().foods;
+
+  useEffect(() => {
+    if (foods) {
+      setFoods(foods.docs);
+    }
+  }, [foods]);
+
+  useEffect(() => {
+    if (auth) {
+      setCurUser(auth);
+    }
+  }, [auth]);
+  return <></>;
+}
--- context/useContext.js
+++ context/useContext.js
@@ -4,11 +4,16 @@ const AppContext = createContext();
 
 export function AppWrapper({ children }) {
   const [foods, setFoods] = useState({});
+  const [curUser, setCurUser] = useState({});
   const [users, setUsers] = useState({});
 
   return (
     <AppContext.Provider
-      value={{ foods: [foods, setFoods], users: [users, setUsers] }}
+      value={{
+        foods: [foods, setFoods],
+        users: [users, setUsers],
+        curUser: [curUser, setCurUser]
+      }}
     >
       {children}
     </AppContext.Provider>
