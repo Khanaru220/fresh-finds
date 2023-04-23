b7de60a3a7d15007b741630f3139c49c5a98f8ad
tin33392js <calsharp0202@yahoo.com>
2023-04-23 18:50:25 +0700

    feature:

--- pages/index.js
+++ pages/index.js
@@ -19,15 +19,7 @@ export default function Home() {
       {!loading && user && (
         <>
           <h1>Welcome to Fresh Find</h1>
-          <nav>
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
-              <Link href={'/app/foods'}>Food</Link>
-            </button>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-              <Link href={'/app/foods'}>Food_dev</Link>
-            </button>
-            <Link href={'/app/foods'}>Food_dev</Link>
-          </nav>
+          <RouteOnClient path="/app/foods" />
         </>
       )}
     </>
